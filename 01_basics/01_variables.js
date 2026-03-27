4a8217f55965363001248dbbe8d00259c2546252
kshitijbhosale9700 <kshitijbhosale9700@gmail.com>
2026-03-27 17:42:06 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId=12415362
+let accountEmail= "kshitijbhosale9700.com"
+var accountPassword = "12345"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword]);
